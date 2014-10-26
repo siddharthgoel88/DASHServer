55db83e2265fabab86bc0106b7b7fe270e536b49
Siddharth Goel <siddharth98391@gmail.com>
2014-10-26 15:19:12 +0800

    Revert "Upload working."
    This reverts commit 26672744e6668a14486c3d29d4ff4d537669d8c3.

--- upload.php
+++ upload.php
@@ -15,34 +15,33 @@ function getname($name) {
         }
     }
 
-//    system('chcon -R -t httpd_sys_rw_content_t ' . $dir);
+    system('chcon -R -t httpd_sys_rw_content_t ' . $dir, $retVal);
     return $dir . $name;
 }
 
-$uploadfile = getname($_FILES['uploaded']['name']);
+$uploadfile = getname($_FILES['upfile']['name']);
 
-system("echo '$uploadfile' > upload/test.txt");
+if (move_uploaded_file($_FILES['upfile']['tmp_name'], $uploadfile)) {
 
-if (move_uploaded_file($_FILES['uploaded']['tmp_name'], $uploadfile)) {
     echo "<h2><font color=#00ff00>Success!</font></h2><br><br>";
 } else {
+
     echo "<h2><font color=#ff0000>Failed!</font></h2><br><br>";
 }
 
-
 echo "File Info:
-    <br><br>File Name:" . $_FILES['uploaded']['name'] .
- "<br><br>Type:" . $_FILES['uploaded']['type'] .
- "<br><br>Temp File Name:" . $_FILES['uploaded']['tmp_name'] .
- "<br><br>Size:" . $_FILES['uploaded']['size'] / 1024 . "K" .
- "<br><br>Error:" . $_FILES['uploaded']['error'];
+    <br><br>File Name:" . $_FILES['upfile']['name'] .
+ "<br><br>Type:" . $_FILES['upfile']['type'] .
+ "<br><br>Temp File Name:" . $_FILES['upfile']['tmp_name'] .
+ "<br><br>Size:" . $_FILES['upfile']['size'] / 1024 . "K" .
+ "<br><br>Error:" . $_FILES['upfile']['error'];
 
 //convert video quality and save
-$name = strtolower(substr($_FILES['uploaded']['name'], 0, (strpos($_FILES['uploaded']['name'], '.'))));
+$name = strtolower(substr($_FILES['upfile']['name'], 0, (strpos($_FILES['upfile']['name'], '.'))));
 
 if (mkdir($dir . $name, 0777)) {
     foreach ($qosArray as $bitrate) {
-        system('/user/local/bin/convert.sh ' . $_FILES['uploaded']['tmp_name'] . ' ' . $bitrate . ' 29.97 320x240 44100 64 ' . $dir . $name . "/" . $bitrate . '.m3u8');
+        system('/user/local/bin/convert.sh ' . $_FILES['upfile']['tmp_name'] . ' ' . $bitrate . ' 29.97 320x240 44100 64 ' . $dir . $name . "/" . $bitrate . '.m3u8');
     }
 } else {
     echo "<br>cannot mkdir for the video.<br>";
